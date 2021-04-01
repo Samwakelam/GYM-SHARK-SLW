1204cae586adab632745b526af41e5a0b0fdeb2e
Improbdrive <sam.wakelam@hotmail.com>
2021-04-01 16:09:53 +0100

    basic area splitting with routing

--- src/components/sections/Main.js
+++ src/components/sections/Main.js
@@ -1,18 +1,18 @@
 // Packages
-import React, { useContext, useEffect, useState } from 'react';
-import ReactHtmlParser from 'react-html-parser';
+import React, { useEffect, useState } from 'react';
+import { Route } from 'react-router-dom';
 
 // Styles
 import './sections.css';
 
-// Context
-import GenderContext, { GenderContextProvider } from '../../context/GenderContext';
-
 // functions
 import fetcher from '../../functions/fetcher';
 
-// components
-import Card from '../module/Card';
+import ExerciseNavbar from '../navigation/ExerciseNavbar';
+
+// pages
+import AllExercise from '../../pages/AllExercise';
+import TypeExercise from '../../pages/TypeExercise';
 
 
 const Main = () => {
@@ -20,24 +20,17 @@ const Main = () => {
   // state
   const [exerciseData, setExerciseData] = useState([]);
 
-  // context
-  const { gender } = useContext(GenderContext);
-
   const fetchData = async () => {
-
     const fetchResponse = await fetcher();
     // console.log('fetchResponse =', fetchResponse);
     setExerciseData(fetchResponse);
   };
 
-
   useEffect(() => {
     fetchData();
-
     return () => {
       console.log('I did Unmount');
     }
-
   }, []);
 
   // console.log('exerciseData =', exerciseData);
@@ -45,23 +38,53 @@ const Main = () => {
 
   return (
     <main>
-      { exerciseData.map((exercise) => {
-        return(
-          <Card key={exercise?.id} bodyAreas={exercise?.bodyAreas}>
-            <img
-              alt='exercise'
-              src={gender === 'male' ? exercise?.male.image : exercise?.female.image}
-              className='exercise-img'
-            />
-            <div>
-              <h2>{exercise?.name}</h2>
-              <div className='transcript'>
-                {ReactHtmlParser(exercise?.transcript)}
-              </div>
-            </div>
-          </Card>
-        );
-      })}
+
+      <section id='filter'>
+        <img
+          alt='gym shark branding'
+          src={`${process.env.PUBLIC_URL}/assets/gs-logo-long.png`}
+          className='logo-long'
+        />
+
+        <select name='bodyAreas' id='bodyAreas-filter' >
+          <option value='all' >All Areas</option>
+          <option value='arms' >Arms</option>
+        </select>
+      </section>
+
+      <div>
+        <ExerciseNavbar />
+        <Route
+          exact path='/'
+          render={(props) => <AllExercise {...props} exerciseData={exerciseData} />}
+        />
+        <Route
+          exact path='/arms'
+          render={(props) => <TypeExercise {...props} exerciseData={exerciseData} type={['Arms', 'Biceps']} />}
+        />
+        <Route
+          exact path='/shoulders'
+          render={(props) => <TypeExercise {...props} exerciseData={exerciseData} type={['Shoulders']} />}
+        />
+        <Route
+          exact path='/back'
+          render={(props) => <TypeExercise {...props} exerciseData={exerciseData} type={['Back']} />}
+        />
+        <Route
+          exact path='/chest'
+          render={(props) => <TypeExercise {...props} exerciseData={exerciseData} type={['Chest']} />}
+        />
+        <Route
+          exact path='/core'
+          render={(props) => <TypeExercise {...props} exerciseData={exerciseData} type={['Core']} />}
+        />
+        <Route
+          exact path='/legs'
+          render={(props) => <TypeExercise {...props} exerciseData={exerciseData} type={['Legs', 'Calves', 'Glutes']} />}
+        />
+
+      </div>
+
     </main>
   )
 }
